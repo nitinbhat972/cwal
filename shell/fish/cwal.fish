404a2e40d36ca7fdd54add76a52bf6df202d88ec
nitinbhat972 <nitinbhat972@gmail.com>
2026-03-13 18:04:19 +0530

    feat(cli): add bash, zsh and fish completions

---
+++ shell/fish/cwal.fish
@@ -0,0 +1,85 @@
+#
+#  cwal: Blazing-fast pywal-like color palette generator written in C.
+#  Copyright (c) 2026 Nitin Bhat <nitinbhat972@gmail.com>
+#  Repository: https://github.com/nitinbhat972/cwal
+#
+#  Licensed under the GNU General Public License v3.0.
+#  If you find this code useful, please consider giving it a star on GitHub!
+#  Any contributions or forks must retain this original header.
+#
+
+complete -c cwal -f
+
+function __fish_cwal_themes
+    set -l themes random_dark random_light random_all
+    
+    set -l config_home "$XDG_CONFIG_HOME"
+    if test -z "$config_home"
+        set config_home "$HOME/.config"
+    end
+    
+    set -l data_home "$XDG_DATA_HOME"
+    if test -z "$data_home"
+        set data_home "$HOME/.local/share"
+    end
+
+    set -l search_dirs "$config_home/cwal/themes" "$data_home/cwal/themes"
+    
+    set -l xdg_dirs "$XDG_DATA_DIRS"
+    if test -z "$xdg_dirs"
+        set xdg_dirs "/usr/local/share:/usr/share"
+    end
+    
+    for dir in (string split : "$xdg_dirs")
+        set -a search_dirs "$dir/cwal/themes"
+    end
+
+    for base in $search_dirs
+        for mode in dark light
+            set -l dir "$base/$mode"
+            if test -d "$dir"
+                for theme in (ls "$dir" | sed 's/\.cwal$//')
+                    echo $theme
+                end
+            end
+        end
+    end
+    for t in $themes
+        echo $t
+    end
+end
+
+function __fish_cwal_backends
+    echo cwal
+    echo libimagequant
+    
+    set -l config_home "$XDG_CONFIG_HOME"
+    if test -z "$config_home"
+        set config_home "$HOME/.config"
+    end
+    
+    set -l dir "$config_home/cwal/backends"
+    if test -d "$dir"
+        for script in (ls "$dir" | sed 's/\.lua$//')
+            echo $script
+        end
+    end
+end
+
+complete -c cwal -l mode -d "Set theme mode (required: <dark|light>)" -r -xa "dark light"
+complete -c cwal -l cols16-mode -d "Set 16-color mode (required: <darken|lighten>)" -r -xa "darken lighten"
+complete -c cwal -l saturation -d "Overall saturation (required: <float>)" -r
+complete -c cwal -l contrast -d "Contrast ratio (required: <float>)" -r
+complete -c cwal -l alpha -d "Alpha transparency (required: <float>)" -r
+complete -c cwal -l out-dir -d "Output directory (required: <path>)" -r -xa "(__fish_complete_directories)"
+complete -c cwal -l backend -d "Processing backend (required: <name>)" -r -xa "(__fish_cwal_backends)"
+complete -c cwal -l img -d "Specify image path (required: <path>)" -r -xa "(__fish_complete_suffix .jpg .jpeg .png .gif .webp)"
+complete -c cwal -l script -d "Run custom script (required: <path>)" -r
+complete -c cwal -l theme -d "Select a theme (required: <name>)" -r -xa "(__fish_cwal_themes)"
+complete -c cwal -l random -d "Select random image (optional: [directory])" -xa "(__fish_complete_directories)"
+complete -c cwal -l no-reload -d "Disable reloading"
+complete -c cwal -l list-backends -d "List available backends"
+complete -c cwal -l list-themes -d "List available themes"
+complete -c cwal -l quiet -d "Suppress all output"
+complete -c cwal -l preview -d "Preview palette"
+complete -c cwal -l help -d "Display help"
