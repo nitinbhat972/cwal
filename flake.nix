353f6361431807e61e0b252be255b4db69b50cb6
nitinbhat972 <nitinbhat972@gmail.com>
2026-03-14 23:15:01 +0530

    chore: update flake.nix to 0.7.0

--- flake.nix
+++ flake.nix
@@ -6,59 +6,52 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      packages.default = pkgs.stdenv.mkDerivation {
-        pname = "cwal";
-        version = "0.5.0";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "cwal";
+          version = "0.7.0";
+          src = ./.;
 
-        src = ./.;
+          nativeBuildInputs = with pkgs; [
+            cmake
+            pkg-config
+          ];
 
-        nativeBuildInputs = with pkgs; [
-          cmake
-          pkg-config
-        ];
+          buildInputs = with pkgs; [
+            imagemagick
+            libimagequant
+            lua
+          ];
 
-        buildInputs = with pkgs; [
-          imagemagick
-          libimagequant
-          lua
-        ];
+          cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
 
-        cmakeFlags = [
-          "-DCMAKE_BUILD_TYPE=Release"
-        ];
-
-        # Use standard install phase from CMake
-        # No need for manual cp if CMakeLists.txt has install() rules
-        
-        meta = with pkgs.lib; {
-          description = "Blazing-fast pywal-like color palette generator written in C";
-          homepage = "https://github.com/nitinbhat972/cwal";
-          license = licenses.gpl3Only;
-          platforms = platforms.linux ++ platforms.darwin;
-          mainProgram = "cwal";
+          meta = with pkgs.lib; {
+            description = "Blazing-fast pywal-like color palette generator written in C";
+            homepage = "https://github.com/nitinbhat972/cwal";
+            license = licenses.gpl3Only;
+            platforms = platforms.linux ++ platforms.darwin;
+            mainProgram = "cwal";
+          };
         };
-      };
 
-      devShells.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          cmake
-          pkg-config
-        ];
-        buildInputs = with pkgs; [
-          imagemagick
-          libimagequant
-          lua
-          gdb
-          valgrind
-        ];
-      };
-    });
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cmake
+            pkg-config
+          ];
+          buildInputs = with pkgs; [
+            imagemagick
+            libimagequant
+            lua
+            gdb
+            valgrind
+          ];
+        };
+      }
+    );
 }
